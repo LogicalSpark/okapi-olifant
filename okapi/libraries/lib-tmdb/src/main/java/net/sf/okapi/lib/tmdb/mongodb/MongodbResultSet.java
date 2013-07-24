ae8869353bbaab4d3c9e81debb5201e794f9664f
Yves <yves.savourel@gmail.com>
2013-07-24 12:12:28 +0200

    Updated libraries versions.

--- okapi/libraries/lib-tmdb/src/main/java/net/sf/okapi/lib/tmdb/mongodb/MongodbResultSet.java
+++ okapi/libraries/lib-tmdb/src/main/java/net/sf/okapi/lib/tmdb/mongodb/MongodbResultSet.java
@@ -1280,4 +1280,16 @@ public class MongodbResultSet implements ResultSet {
 		return null;
 	}
 
+	@Override
+	public <T> T getObject(int arg0, Class<T> arg1) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T getObject(String arg0, Class<T> arg1) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
